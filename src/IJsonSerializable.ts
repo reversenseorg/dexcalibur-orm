845c4f113b831f79a97dadb2607a437c935f2623
Georges-B. Michel <georges@reversense.com>
2023-12-11 02:46:52 +0100

    Add "include only fields" options to "toJsonObject"

--- src/IJsonSerializable.ts
+++ src/IJsonSerializable.ts
@@ -3,6 +3,12 @@ import {IStringIndex} from "./core/IStringIndex.js";
 export interface SerializeOptions {
   override?: IStringIndex<any>,
   exclude?: IStringIndex<any>,
+  /**
+   * A list of name of fiel to include into the value returned.
+   * Others fields are ignored.
+   * @type {string[]}
+   */
+  include?: string[]
 }
 
 export interface IJsonSerializable extends IStringIndex<any>{
