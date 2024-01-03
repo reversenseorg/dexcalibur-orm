0bac4ae3ec08f8f7e00e7701f771872584f90cac
Georges-B. Michel <georges@reversense.com>
2024-01-03 04:33:47 +0100

    Add unserialize method to TagCategory

--- entry.ts
+++ entry.ts
@@ -39,7 +39,7 @@ TagCategory.TYPE.updateProperties([
     (new NodeProperty('name')).type(DbDataType.STRING).key(DbKeyType.PRIMARY).notnull(),
     (new NodeProperty('descr')).type(DbDataType.STRING),
     (new NodeProperty('_tags')).volatile().multiple(Tag.TYPE),
-    (new NodeProperty("tags")).type(DbDataType.STRING).def([]),
+    (new NodeProperty("tags")).type(DbDataType.STRING).def("[]"),
 ]).builder(TagCategory);
 
 export {
